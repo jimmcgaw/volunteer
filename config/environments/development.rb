d6922214a0695c9cdca95054c8729185545a13ee
Jim McGaw <jpmcgaw@gmail.com>
2011-02-26 21:32:35 -0800

    Change name of module for entire project, since 'Volunteer' is a word that will be claimed by a Model class

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Volunteer::Application.configure do
+Volevent::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
